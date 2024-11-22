bdeb3657abce2dcadde6befdb39c6de57ce6cfbc
dorwp <dorianwill119@gmail.com>
2024-11-22 00:52:53 -0500

    Final

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,31 @@
 {
     public class Program
     {
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            
+            int division = a / b;
+            int modulus = a % b;
+            
+            Console.WriteLine($"{a}/{b} is {division} remainder {modulus}");
+            
+            //Exercise 2
+            Console.WriteLine("Input the radius of a circle:");
+            double radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of the circle with radius of {radius} is {area}");
         }
     }
 }
--- OperatorExercise/obj/Debug/net8.0/OperatorExercise.AssemblyInfo.cs
+++ OperatorExercise/obj/Debug/net8.0/OperatorExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperatorExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d2d1b8f262369534f5740c99b29542f2d33f8b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+719890e61732b9e57c3d4fb1cb89103c5219bbe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperatorExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperatorExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
